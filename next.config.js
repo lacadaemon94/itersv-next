6c8bf969ffb049c98010116741eb199815e45dbe
lacadaemon94 <famflores10@msn.com>
2023-07-18 16:35:04 -0600

    modified:   next.config.js 	modified:   src/app/styles/app.css

--- next.config.js
+++ next.config.js
@@ -37,6 +37,18 @@ async function headers() {
                     key: "X-XSS-Protection",
                     value: "1; mode=block",
                 },
+                {
+                    key: "Referrer-Policy",
+                    value: "strict-origin-when-cross-origin",
+                },
+                {
+                    key: "Permissions-Policy",
+                    value: "geolocation=(), microphone=(), camera=()",
+                },
+                {
+                    key: "Content-Security-Policy",
+                    value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'",
+                }
             ],
         },
     ];
