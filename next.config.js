a6e667230769139c918b27f8ff0c723d8d7c418d
lacadaemon94 <famflores10@msn.com>
2023-07-18 16:39:14 -0600

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -47,7 +47,7 @@ async function headers() {
                 },
                 {
                     key: "Content-Security-Policy",
-                    value: "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self'",
+                    value: "default-src 'self' https://itersv.com/; script-src 'self' 'unsafe-inline' https://itersv.com/; style-src 'self' 'unsafe-inline' https://itersv.com/; img-src 'self' https://itersv.com/ data:; connect-src 'self'; manifest-src 'self' https://itersv.com/",
                 }
             ],
         },
